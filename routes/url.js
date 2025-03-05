f9b2f05c3ef6d98225cac60c48d1b4196ec7eb70
aartibhutra <aartimaheshwari160@gmail.com>
2025-03-05 15:19:41 +0530

    update routes file

--- routes/url.js
+++ routes/url.js
@@ -1,3 +1,8 @@
 const express = require('express');
-
+// import :
+const {handleGenerateNewShortURL} = require('../controllers/url');
 const router = express.Router();
+
+router.post("/", handleGenerateNewShortURL);
+
+module.exports = router;
