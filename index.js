5f0b1d56d140f5ecb58b61e7b185da07e24939f5
aartibhutra <aartimaheshwari160@gmail.com>
2025-03-05 16:18:09 +0530

    add middleware

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
-
+const {connectToMongoDB} = require('./connect');
 const urlRoute = require('./routes/url');
+
 const app = express();
 const PORT = 8000;
+
+connectToMongoDB("mongodb://127.0.0.1:27017/short-url")
+.then(() => console.log('Mongodb Connected'));
+
+//MiddleWare: pass incoming bodies 
+app.use(express.json());
 
 app.use("/url" ,urlRoute);
 
