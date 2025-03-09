ba7bf357c1b2c767200ad49482620beca68312e7
aartibhutra <aartimaheshwari160@gmail.com>
2025-03-09 15:17:23 +0530

    render homePage

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -2,4 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
+// '/' means the root of the application (home page)
+router.get('/', (req, res) => {
+    return res.render("home");
+});
+
 module.exports = router;
