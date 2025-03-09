a572116450cad53ab2f4790bca050c6a84f8d8d6
aartibhutra <aartimaheshwari160@gmail.com>
2025-03-09 14:02:35 +0530

    html

--- index.js
+++ index.js
@@ -13,14 +13,26 @@ connectToMongoDB("mongodb://127.0.0.1:27017/short-url")
 app.use(express.json());
 
 // route : server side rendering 
-app.get('/test' , (req,res) => {
-    return res.end('<h1>Hey From Server</h1>');
+app.get('/test' , async (req,res) => {
+    const allUrls = await URL.find({});
+    return res.end(
+        `
+        <html>
+            <head></head>
+            <body>
+                <ol>
+                    ${allUrls.map(url => `<li>${url.shortId} - ${url.redirectURL} - ${url.visitHistory.length} </li>`).join('')}
+                </ol>
+            </body>
+        </html>
+        `
+    )
 });
 
 app.use("/url" ,urlRoute);
 
 // for dynamic route :
-app.get('/:shortId', async (req , res) => {
+app.get('/url/:shortId', async (req , res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate(
         {
