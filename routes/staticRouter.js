d9e9932f42e5d4614f2d49f99a6e99d85f864f8c
aartibhutra <aartimaheshwari160@gmail.com>
2025-03-09 15:29:15 +0530

    add table

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -3,8 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 // '/' means the root of the application (home page)
-router.get('/', (req, res) => {
-    return res.render("home");
+router.get('/', async (req, res) => {
+    const allurls = await URL.find({});
+    return res.render("home",{
+        urls: allurls,
+    });
 });
 
 module.exports = router;
