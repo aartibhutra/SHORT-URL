bf876281b2361bacd7efea67fbdbf25129d45619
aartibhutra <aartimaheshwari160@gmail.com>
2025-03-10 09:19:08 +0530

    add changes

--- models/user.js
+++ models/user.js
@@ -1,1 +1,25 @@
 // use for authentication 
+
+const mongoose = require('mongoose');
+
+// Schema :
+const userSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: true,
+    },
+    email : {
+        type: String,
+        required: true,
+        unique : true,
+    },
+    password:{
+        type: String,
+        required: true,
+    },
+} , {timestamps : true}
+);
+
+const User = mongoose.model('user' , userSchema);
+
+module.exports = User;
