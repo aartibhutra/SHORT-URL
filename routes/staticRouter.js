8c446e57fb41968181ae59679f861b16dbb36ba7
aartibhutra <aartimaheshwari160@gmail.com>
2025-04-01 09:51:56 +0530

    login

--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,18 +1,22 @@
 const express = require('express');
-const urls = require('../models/url');
+const URL= require('../models/url');
+
 const router = express.Router();
 
 // '/' means the root of the application (home page)
 router.get('/', async (req, res) => {
-    const allurls = await urls.find({});
-    res.locals.urls = allUrls;
+    const allurls = await URL.find({});
+    // res.locals.urls = allUrls;
     return res.render("home",{
-        url: allurls,
+        urlS: allurls,
     });
 });
 
 router.get("/signup" , (req , res) =>{
     return res.render("signup");
+});
+router.get("/login" , (req , res) =>{
+    return res.render("login");
 });
 
 module.exports = router;
